91d582705a75eea21d69b7e9504d8cb5db5023dd
Aaron Kling <webgeek1234@gmail.com>
2021-08-10 11:11:16 -0500

    atv: Allow setupwraith configs to be set by vendor
    Change-Id: I0e2831019d5bbb9d432bbad48cd91c98f3c3cb6f
    (cherry picked from commit 72049da6bc559ad5e57a2824b975fba5102258a0)
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -33,3 +33,8 @@ endif
 
 # Selectively include legacy rules defined by the products
 -include device/aosp/sepolicy/legacy-common/sepolicy.mk
+
+# Include atv rules on atv product
+ifeq ($(PRODUCT_IS_ATV), true)
+include device/aosp/sepolicy/atv/sepolicy.mk
+endif
